c9ec2dc44d549b238a1a72dd2a8b2acdc36ac94f
Purva211 <purvadev2@gmail.com>
2026-08-17 13:41:23 +0530

    Update Register.jsx

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -4,7 +4,7 @@ import { Link, useNavigate } from "react-router-dom";
 
 import api from "../services/api";
 
-import "../Auth.css";
+import "../auth.css";
 
 const Register = () => {
   const navigate = useNavigate();
