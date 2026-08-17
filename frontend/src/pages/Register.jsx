af0c55cf5e5c487e5c092aacc155f10c68399f06
Purva211 <purvadev2@gmail.com>
2026-08-17 13:43:27 +0530

    Change import path for auth.css in Register.jsx

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -4,7 +4,7 @@ import { Link, useNavigate } from "react-router-dom";
 
 import api from "../services/api";
 
-import "../auth.css";
+import "./auth.css";
 
 const Register = () => {
   const navigate = useNavigate();
